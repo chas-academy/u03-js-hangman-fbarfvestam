c2a3a8d31e2891ea9f747f1ea5258e9f384d7728
fbarfvestam <filip.barfvestam@chasacademy.se>
2021-11-25 16:27:21 +0100

    fixed function for buttons inactive/active ane and restart function started

--- js/script.js
+++ js/script.js
@@ -16,14 +16,17 @@ const wordList = [
 ];      
 
 let correctLetter = [];
-
+const maxGuesses = 6;
 let startGameBtnEl = document.querySelector("#startGameBtn") // DOM-nod: knappen som du startar spelet med
 let msgHolderEl = document.querySelector("#message"); // DOM-nod: Ger meddelande när spelet är över
+const resetButton = document.querySelector(".resetButton");
 let guesses = 0; // Number: håller antalet gissningar som gjorts
 let letterButtonEls = document.querySelectorAll("#letterButtons button"); // Array av DOM-noder: Knapparna för bokstäverna
 let letterBoxEls = document.querySelector("#letterBoxes > ul"); // Array av DOM-noder: Rutorna där bokstäverna ska stå
+letterBoxEls.innerHTML="";
 let hangmanImgEl = document.querySelector("#hangman"); // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
 let selectedWord = ""; // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+
 
 
 startGameBtnEl.addEventListener("click", startGame); 
@@ -35,6 +38,7 @@ function startGame() {
   generateRandomWord();
   createLetterBoxes();
   clickButton();
+  resetButton.addEventListener("click", restartGame);
 }
 
 // Funktion som slumpar fram ett ord
@@ -47,7 +51,6 @@ function generateRandomWord() {
 // Funktion som tar fram bokstävernas rutor, antal rutor beror på vilket ord slumptas fram
 
 function createLetterBoxes() {
-    letterBoxEls.innerHTML="";
    for(let i=0; i < selectedWord.length; i++ ) {
    letterBoxEls.innerHTML+= `<li><input disabled value="_" type="text"></li>`;
   } 
@@ -58,8 +61,9 @@ function createLetterBoxes() {
  function clickButton() { 
   letterBoxEls = document.querySelectorAll("#letterBoxes > ul > li > input");
   for (let i = 0; i < letterButtonEls.length; i++) {
+    guesses=0;
     letterButtonEls[i].addEventListener("click", ()=> {
-        for(let j = 0; j < selectedWord.length; j++) {
+         for(let j = 0; j < selectedWord.length; j++) {
             if (letterButtonEls[i].value.toLowerCase() === selectedWord[j].toLowerCase()) {
                 console.log('Log for when word is correct',letterButtonEls[i].value);
                 letterBoxEls[j].value = selectedWord[j];
@@ -68,80 +72,31 @@ function createLetterBoxes() {
         if (!selectedWord.includes(letterButtonEls[i].value.toLowerCase())) {
             guesses+=1;
             hangmanImgEl.src=`images/h${guesses}.png`;
-        }
-    });
-  }
+            checkLoss();
+          } 
+        },{ once: true }); // Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på 
+      }
+    }
+    function checkLoss(){
+      if (guesses === maxGuesses) {
+         console.log("du förlorade");
+        msgHolderEl.style.display="flex";
+        guesses=0;
+     //   indexOfbtn.removeEventListener("click", ()=> {});
+      }
 }
 
-function checkFail ()
 
+function restartGame() {
+  startGameBtnEl.addEventListener("click", startGame); 
+  msgHolderEl.style.display="none";
+  selectedWord = "";
+  letterBoxEls = document.querySelector("#letterBoxes > ul");
+  letterBoxEls.innerHTML="";
+  guesses=0;
+  hangmanImgEl.src=`images/h`+ guesses + ".png";
+  startGameBtnEl.removeAttribute("disabled");
+}
 
 // Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
 
-// Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på 
-
-
-
-/*
-const clickCheck = (e) => { //sätt funktioner till const då du inte behöver ändra dem 
-  // gameProgress(e.target.value);
-  letterBoxEls = document.querySelectorAll("#letterBoxes > ul > li > input");
-  console.log(e.target.value);
-  for (let i = 0; i < selectedWord.length; i++) {
-    if (e.target.value.toLowerCase() === selectedWord[i].toLowerCase()) {
-      letterBoxEls[i].value = e.target.value;
-      correctLetter.push(e.target.value);
-    } 
-  }
-  testFail(e.currentTarget.value);
-/*   for(let i=0; i < letterButtonEls.length; i++) {
-    for(let j=0; j < selectedWord.length; j++) {
-      if(letterButtonEls[i].value.toLowerCase() != selectedWord[j].toLowerCase()) {
-        alert("hej");
-      }
-    }
-  } */
-/*   if (selectedWord.indexOf(e.target.value) === -1) {
-    alert("hej");
-   // guesses++;
-   // hangmanImgEl.src = `images/h${guesses}.png`;
-
-    //du skulle även behöva ta bort eventListener på knappen här om du vill undvika att det går att klicka igen. 
-  } 
-  e.target.setAttribute("disabled", true);
-  console.log(guesses);
-  console.log(correctLetter);
-};	
-
-function testFail (clickedButton) {
-  if (selectedWord.indexOf(clickedButton) < 0) {
-    alert(clickedButton);
-/*     guesses++; 
-    hangmanImgEl.src = `images/h${guesses}.png`; 
-  }
-
-}
-
-
-
-/* function clickButton() {
-  let clickCheck = (e)=> {
-     letterBoxEls = document.querySelectorAll("#letterBoxes > ul > li > input");
-    console.log(e.target.value);
-    for(let i=0; i < selectedWord.length; i++ ) {
-      if(e.target.value.toLowerCase()===selectedWord[i].toLowerCase()) {
-        letterBoxEls[i].value=e.target.value;
-      }
-    }
-    if(selectedWord.indexOf(e.target.value)===-1) { 
-      guesses++; 
-      hangmanImgEl.src=`images/h${guesses}.png`;
-      }
-    console.log(guesses);
-   }
-
-  for(let i=0; i < letterButtonEls.length; i++ ) {
-    letterButtonEls[i].addEventListener("click", clickCheck);
-  } console.log(letterBoxEls);
-  }
- */
